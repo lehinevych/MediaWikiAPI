0d1b1b4a6a71b2bace3f24b50bb1f78721bac312
Daniel Underwood <daniel.underwood13@gmail.com>
2018-04-08 12:23:11 -0400

    Handle cases where argv isn't as expected

--- mediawikiapi/__init__.py
+++ mediawikiapi/__init__.py
@@ -1,10 +1,9 @@
 import sys
 
-if not (sys.argv[0] == 'pip' or (len(sys.argv) > 1 and sys.argv[1] == 'setup.py')):
+if not (sys.argv[0] in ['pip', 'setup.py'] or (len(sys.argv) > 1 and sys.argv[1] in ['setup.py', 'egg_info'])):
     from .config import *
     from .mediawikiapi import *
     from .exceptions import *
     from .language import *
-
 
 __version__ = "1.1.2"
