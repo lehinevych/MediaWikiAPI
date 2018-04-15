3faf4d2ae4616a0c8356277cbc8a59a7442df3f5
Taras Lehinevych <info@taraslehinevych.me>
2018-04-15 18:46:08 +0300

    nuke import version from setup py

--- mediawikiapi/__init__.py
+++ mediawikiapi/__init__.py
@@ -1,10 +1,5 @@
-import sys
-
-if not sys.argv[0] == 'pip' and not sys.argv[1] == 'setup.py':
-    from .config import *
-    from .mediawikiapi import *
-    from .exceptions import *
-    from .language import *
-
-
-__version__ = "1.1.2"
+from .config import *
+from .mediawikiapi import *
+from .exceptions import *
+from .language import *
+from .version import *
