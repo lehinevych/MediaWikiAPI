57a8c1ea62827408c03aa9ccf51c630e32c063d5
Taras Lehinevych <tleginevych@gmail.com>
2021-04-23 00:30:11 +0100

    fixing test data

--- tests/page_test.py
+++ tests/page_test.py
@@ -32,7 +32,9 @@ class TestPageSetUp(unittest.TestCase):
         mp = api.page("Template:cn", auto_suggest=False)
 
         self.assertEqual(mp.title, "Template:Citation needed")
-        self.assertEqual(mp.url, "https://en.wikipedia.org/wiki/Template:Citation_needed")
+        self.assertEqual(
+            mp.url, "https://en.wikipedia.org/wiki/Template:Citation_needed"
+        )
 
     def test_redirect_false(self):
         """Test that page raises an error on a redirect when redirect == False."""
@@ -64,9 +66,17 @@ class TestPageSetUp(unittest.TestCase):
     def test_disambiguate(self):
         """Test that page raises an error when a disambiguation page is reached."""
         page = api.page("Template", auto_suggest=False, redirect=False)
-        disambiguation_list = [u'Template (file format)', u'Template (C++)', u'Template metaprogramming',
-                               u'Template method pattern', u'Template processor', u'Template (word processing)',
-                               u'Web template', u'Template (racing)', u'Template (novel)']
+        disambiguation_list = [
+            u"Template (file format)",
+            u"Template (C++)",
+            u"Template metaprogramming",
+            u"Template method pattern",
+            u"Template processor",
+            u"Template (word processing)",
+            u"Web template",
+            u"Template (racing)",
+            u"Template (novel)",
+        ]
         for disambiguation_opt in disambiguation_list:
             self.assertTrue(disambiguation_opt in page.disambiguate_pages)
 
@@ -81,6 +91,7 @@ class TestPageSetUp(unittest.TestCase):
 
 class TestPage(unittest.TestCase):
     """Test the functionality of the rest of mediawikiapi.page."""
+
     maxDiff = None
 
     def setUp(self):
@@ -102,50 +113,64 @@ class TestPage(unittest.TestCase):
     def test_url(self):
         """Test the url."""
         self.assertEqual(self.celtuce.url, "https://en.wikipedia.org/wiki/Celtuce")
-        self.assertEqual(self.cyclone.url, "https://en.wikipedia.org/wiki/2007_Atlantic_hurricane_season")
+        self.assertEqual(
+            self.cyclone.url,
+            "https://en.wikipedia.org/wiki/2007_Atlantic_hurricane_season",
+        )
 
     def test_content(self):
         """Test the plain text content."""
-        self.assertIn(mock_data['data']["celtuce.content"], self.celtuce.content)
+        self.assertIn(mock_data["data"]["celtuce.content"], self.celtuce.content)
 
     def test_revision_id(self):
         """Test the revision id."""
-        self.assertEqual(self.celtuce.revision_id, mock_data['data']["celtuce.revid"])
-        self.assertEqual(self.cyclone.revision_id, mock_data['data']["cyclone.revid"])
+        self.assertEqual(self.celtuce.revision_id, mock_data["data"]["celtuce.revid"])
+        self.assertEqual(self.cyclone.revision_id, mock_data["data"]["cyclone.revid"])
 
     def test_backlinks(self):
         """Test the backlinks."""
-        self.assertCountEqual(self.celtuce.backlinks, mock_data['data']["celtuce.backlinks"])
-        self.assertCountEqual(self.cyclone.backlinks, mock_data['data']["cyclone.backlinks"])
+        self.assertCountEqual(
+            self.celtuce.backlinks, mock_data["data"]["celtuce.backlinks"]
+        )
+        self.assertCountEqual(
+            self.cyclone.backlinks, mock_data["data"]["cyclone.backlinks"]
+        )
 
     def test_backlinks_ids(self):
         """Test the backlinks ids."""
-        self.assertCountEqual(self.celtuce.backlinks_ids, mock_data['data']["celtuce.backlinks_ids"])
-        self.assertCountEqual(self.cyclone.backlinks_ids, mock_data['data']["cyclone.backlinks_ids"])
+        self.assertCountEqual(
+            self.celtuce.backlinks_ids, mock_data["data"]["celtuce.backlinks_ids"]
+        )
+        self.assertCountEqual(
+            self.cyclone.backlinks_ids, mock_data["data"]["cyclone.backlinks_ids"]
+        )
 
     def test_parent_id(self):
         """Test the parent id."""
-        self.assertEqual(self.celtuce.parent_id, mock_data['data']["celtuce.parentid"])
-        self.assertEqual(self.cyclone.parent_id, mock_data['data']["cyclone.parentid"])
+        self.assertEqual(self.celtuce.parent_id, mock_data["data"]["celtuce.parentid"])
+        self.assertEqual(self.cyclone.parent_id, mock_data["data"]["cyclone.parentid"])
 
     def test_images(self):
         """Test the list of image URLs."""
-        self.assertCountEqual(self.celtuce.images, mock_data['data']["celtuce.images"])
-        self.assertCountEqual(self.cyclone.images, mock_data['data']["cyclone.images"])
+        self.assertCountEqual(self.celtuce.images, mock_data["data"]["celtuce.images"])
+        self.assertCountEqual(self.cyclone.images, mock_data["data"]["cyclone.images"])
 
     def test_hanging_page_image_query(self):
-        bill_foster_page = api.page('Bill Foster (politician)', preload=True)
-        self.assertCountEqual(bill_foster_page.images, mock_data['data']["bill_foster_page.images"])
+        bill_foster_page = api.page("Bill Foster (politician)", preload=True)
+        self.assertCountEqual(
+            bill_foster_page.images, mock_data["data"]["bill_foster_page.images"]
+        )
 
     def test_references(self):
         """Test the list of reference URLs."""
-        self.assertCountEqual(self.celtuce.references, mock_data['data']["celtuce.references"])
+        self.assertCountEqual(
+            self.celtuce.references, mock_data["data"]["celtuce.references"]
+        )
 
     def test_links(self):
         """Test the list of titles of links to Wikipedia pages."""
-        self.assertCountEqual(self.celtuce.links, mock_data['data']["celtuce.links"])
-        self.assertCountEqual(self.cyclone.links, mock_data['data']["cyclone.links"])
-
+        self.assertCountEqual(self.celtuce.links, mock_data["data"]["celtuce.links"])
+        self.assertCountEqual(self.cyclone.links, mock_data["data"]["cyclone.links"])
 
     def test_html(self):
         """Test the full HTML method."""
@@ -154,50 +179,75 @@ class TestPage(unittest.TestCase):
     def test_coordinates(self):
         """Test geo coordinates of a page"""
         lat, lon = self.great_wall_of_china.coordinates
-        self.assertEqual(str(lat.quantize(Decimal('1.000'))), mock_data['data']['great_wall_of_china.coordinates.lat'])
-        self.assertEqual(str(lon.quantize(Decimal('1.000'))), mock_data['data']['great_wall_of_china.coordinates.lon'])
+        self.assertEqual(
+            str(lat.quantize(Decimal("1.000"))),
+            mock_data["data"]["great_wall_of_china.coordinates.lat"],
+        )
+        self.assertEqual(
+            str(lon.quantize(Decimal("1.000"))),
+            mock_data["data"]["great_wall_of_china.coordinates.lon"],
+        )
 
     def test_summary(self):
         """Test the summary."""
         # Strip is used to nuke \n from the end
-        self.assertIn(mock_data['data']["celtuce.summary"], self.celtuce.summary)
-        self.assertIn(mock_data['data']["cyclone.summary"], self.cyclone.summary)
+        self.assertIn(mock_data["data"]["celtuce.summary"], self.celtuce.summary)
+        self.assertIn(mock_data["data"]["cyclone.summary"], self.cyclone.summary)
 
     def test_categories(self):
         """Test the list of categories of Wikipedia pages."""
-        self.assertCountEqual(self.celtuce.categories, mock_data['data']["celtuce.categories"])
-        self.assertCountEqual(self.cyclone.categories, mock_data['data']["cyclone.categories"])
+        self.assertCountEqual(
+            self.celtuce.categories, mock_data["data"]["celtuce.categories"]
+        )
+        self.assertCountEqual(
+            self.cyclone.categories, mock_data["data"]["cyclone.categories"]
+        )
 
     def test_sections(self):
         """Test the list of section titles."""
-        self.assertCountEqual(self.cyclone.sections, mock_data['data']["cyclone.sections"])
+        self.assertCountEqual(
+            self.cyclone.sections, mock_data["data"]["cyclone.sections"]
+        )
 
     def test_section(self):
         """Test text content of a single section."""
-        self.assertEqual(self.cyclone.section("Seasonal forecasts"), mock_data['data']["cyclone.section.seasonal_forecasts"])
+        self.assertEqual(
+            self.cyclone.section("Seasonal forecasts"),
+            mock_data["data"]["cyclone.section.seasonal_forecasts"],
+        )
         self.assertEqual(self.cyclone.section("History"), None)
 
     def test_lang_title(self):
         """ Test lang_title function"""
-        self.assertEqual(self.celtuce.lang_title('es'), mock_data['data']["celtuce.es_lang"])
-        self.assertEqual(self.cyclone.lang_title('ru'), mock_data['data']["cyclone.ru_lang"])
+        self.assertEqual(
+            self.celtuce.lang_title("es"), mock_data["data"]["celtuce.es_lang"]
+        )
+        self.assertEqual(
+            self.cyclone.lang_title("ru"), mock_data["data"]["cyclone.ru_lang"]
+        )
 
     def test_pageprops(self):
         """Test pageprops of a page"""
-        self.assertEqual(self.celtuce.pageprops, mock_data['data']["celtuce.pageprops"])
-        self.assertEqual(self.cyclone.pageprops, mock_data['data']["cyclone.pageprops"])
+        self.assertEqual(self.celtuce.pageprops, mock_data["data"]["celtuce.pageprops"])
+        self.assertEqual(self.cyclone.pageprops, mock_data["data"]["cyclone.pageprops"])
 
     def test_infobox(self):
         """Test infobox of a page"""
-        self.assertEqual(self.avatar.infobox, mock_data['data']["infobox_avatar"])
+        self.assertEqual(self.avatar.infobox, mock_data["data"]["infobox_avatar"])
 
     def test_category_members(self):
         """Test category members"""
-        self.assertEqual(api.category_members(title='Physics'), mock_data['data']["category_members_physics"])
-        self.assertEqual(api.category_members(pageid=692318), mock_data['data']["category_members_physics"])
+        self.assertEqual(
+            api.category_members(title="Physics"),
+            mock_data["data"]["category_members_physics"],
+        )
+        self.assertEqual(
+            api.category_members(pageid=692318),
+            mock_data["data"]["category_members_physics"],
+        )
         with self.assertRaises(ValueError):
-            api.category_members(title='Wikipedia', pageid=6923181)
+            api.category_members(title="Wikipedia", pageid=6923181)
         with self.assertRaises(ValueError):
             api.category_members(pageid=6923181)
         with self.assertRaises(ValueError):
-            api.category_members(title='')
+            api.category_members(title="")
