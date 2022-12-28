ca535c8b8bc5972bc44735470b86906f81e9defe
Taras Lehinevych <info@taraslehinevych.me>
2022-12-28 01:41:44 +0000

    allow to path the mediawiki url so the client can be easierly reused

--- mediawikiapi/config.py
+++ mediawikiapi/config.py
@@ -1,4 +1,5 @@
 from datetime import timedelta
+from typing import Union
 from .language import Language
 
 
@@ -6,70 +7,82 @@ class Config(object):
     """
     Contains global configuration
     """
-    DEFAULT_USER_AGENT = 'mediawikiapi (https://github.com/lehinevych/MediaWikiAPI/)'
-    DONATE_URL = 'https://donate.wikimedia.org/w/index.php?title=Special:FundraiserLandingPage'
-    API_URL = 'https://{}.wikipedia.org/w/api.php'
 
-    def __init__(self, language=None, user_agent=None, rate_limit=None):
+    DEFAULT_USER_AGENT = "mediawikiapi (https://github.com/lehinevych/MediaWikiAPI/)"
+    DONATE_URL = (
+        "https://donate.wikimedia.org/w/index.php?title=Special:FundraiserLandingPage"
+    )
+    API_URL = "https://{}.wikipedia.org/w/api.php"
+
+    def __init__(
+        self,
+        language: str = None,
+        user_agent: str = None,
+        rate_limit: Union[int, timedelta] = None,
+        mediawiki_url: str = None,
+    ):
         if language is not None:
             self.__lang = Language(language)
         else:
             self.__lang = Language()
         self.__rate_limit_last_call = None
+        if isinstance(rate_limit, int):
+            rate_limit = timedelta(milliseconds=rate_limit)
         self.__rate_limit = rate_limit
         self.timeout = None
         self.user_agent = user_agent or self.DEFAULT_USER_AGENT
+        self.mediawiki_url = mediawiki_url or self.API_URL
 
     @classmethod
-    def donate_url(cls):
-        '''Return media wiki donate url'''
+    def donate_url(cls) -> str:
+        """Return media wiki donate url"""
         return cls.DONATE_URL
 
     @property
-    def language(self):
-        '''Return current global language'''
+    def language(self) -> None:
+        """Return current global language"""
         return self.__lang.language
 
     @language.setter
-    def language(self, language):
-        '''Set a new language
+    def language(self, language: Union[str, Language]) -> None:
+        """Set a new language
         Arguments:
         * language - (string or Language instance) specifying the language
-        '''
+        """
         if isinstance(language, Language):
             self.__lang = language
         else:
             self.__lang.language = language
 
-    def get_api_url(self, language=None):
-        '''Return api for specified language
+    def get_api_url(self, language=None) -> str:
+        """Return api for specified language
         Arguments:
         * language - (string or Language instance) specifying the language
-        '''
+        """
         if language is not None:
             if isinstance(language, Language):
-                return self.API_URL.format(language.language)
+                return self.mediawiki_url.format(language.language)
             else:
                 # does the language verification
                 lang = Language(language)
-                return self.API_URL.format(lang.language)
-        return self.API_URL.format(self.__lang.language)
+                return self.mediawiki_url.format(lang.language)
+        return self.mediawiki_url.format(self.__lang.language)
 
     @property
-    def rate_limit(self):
+    def rate_limit(self) -> timedelta:
         return self.__rate_limit
 
     @property
-    def rate_limit_last_call(self):
+    def rate_limit_last_call(self) -> timedelta:
         return self.__rate_limit_last_call
 
     @rate_limit_last_call.setter
-    def rate_limit_last_call(self, last_call):
+    def rate_limit_last_call(self, last_call: timedelta) -> None:
         self.__rate_limit_last_call = last_call
 
     @rate_limit.setter
-    def rate_limit(self, rate_limit):
-        '''
+    def rate_limit(self, rate_limit: Union[int, timedelta]) -> None:
+        """
         Enable or disable rate limiting on requests to the Mediawiki servers.
         If rate limiting is not enabled, under some circumstances (depending on
         load on Wikipedia, the number of requests you and other `wikipedia` users
@@ -82,7 +95,7 @@ class Config(object):
         * min_wait - (integer or timedelta) describes the minimum time to wait in miliseconds before requests.
                Example timedelta(milliseconds=50). If None, rate_limit won't be used.
 
-        '''
+        """
         if rate_limit is None:
             self.__rate_limit = None
         elif isinstance(rate_limit, timedelta):
